fceb086afd7883fd184708728fef98f631abe628
Michael Kennedy <mikeckennedy@gmail.com>
2016-02-29 15:01:44 +0100

    updated with re-recorded edition.

--- apps/02-guess-number-app/final/program.py
+++ apps/02-guess-number-app/final/program.py
@@ -1,41 +1,26 @@
 import random
 
+print('---------------------------------')
+print('   GUESS THAT NUMBER GAME')
+print('---------------------------------')
+print()
 
-def run_game():
-    print("Guess my number game.")
-    number = random.randint(0, 100)
-    guess = -1
+the_number = random.randint(0, 100)
+guess = -1
 
-    while not guess == number:
-        guess_text = input("Guess a number between 0 and 100: ")
-        guess = int(guess_text)
-        if guess < number:
-            print("Sorry but {0} is LOWER than the number".format(guess))
-        elif guess > number:
-            print("Sorry but {0} is HIGHER than the number".format(guess))
-        else:
-            print("YES! You've got it. The number was {}".format(guess))
+name = input('Player what is your name? ')
 
+while guess != the_number:
+    guess_text = input('Guess a number between 0 and 100: ')
+    guess = int(guess_text)
 
-def print_header():
-    print("---------------------------------------------------------------")
-    print("         GUESS A NUMBER APP - demonstrates: ")
-    print("         * Shape of app (whitespace) ")
-    print("         * Basic methods ")
-    print("         * Booleans ")
-    print("         * Type conversion ")
-    print("         * Import module ")
-    print("         * If ")
-    print("         * While ")
-    print("         * Empty blocks / suites ")
-    print("         * String format ")
-    print("---------------------------------------------------------------")
-    print()
+    if guess < the_number:
+        # print('Your guess of ' + guess + ' was too LOW.')
+        print('Sorry {}, your guess of {} was too LOW.'.format(name, guess))
+    elif guess > the_number:
+        print('Sorry {}, your guess of {} was too HIGH.'.format(name, guess))
+    else:
+        print('Excellent work {}, you won, it was {}!'.format(name, guess))
 
+print('done')
 
-def main():
-    print_header()
-    run_game()
-
-
-main()

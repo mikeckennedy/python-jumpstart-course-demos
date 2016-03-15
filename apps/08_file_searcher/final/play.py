e1e88b2ce2307ce977dba0f1d02d9bd1cdb1c556
Michael Kennedy <mikeckennedy@gmail.com>
2016-03-15 16:45:39 +0100

    app 8 done.

--- apps/08_file_searcher/final/play.py
+++ apps/08_file_searcher/final/play.py
@@ -15,3 +15,57 @@ print("5!={:,}, 3!={:,}, 11!={:,}".format(
     factorial(3),  # 6
     factorial(11)  # HUGE
 ))
+
+
+# Fibonacci numbers:
+# 1, 1, 2, 3, 5, 8, 13, 21, ...
+
+def fibonacci(limit):
+    nums = []
+
+    current = 0
+    next = 1
+
+    while current < limit:
+        current, next = next, next + current
+        nums.append(current)
+
+    return nums
+
+
+print('via lists')
+for n in fibonacci(100):
+    print(n, end=', ')
+
+print()
+
+
+def fibonacci_co():
+    current = 0
+    next = 1
+
+    while True:
+        current, next = next, next + current
+        yield current
+
+
+print('with yield')
+for n in fibonacci_co():
+    if n > 1000:
+        break
+
+    print(n, end=', ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- apps/08_file_searcher/final/program.py
+++ apps/08_file_searcher/final/program.py
@@ -18,13 +18,17 @@ def main():
         return
 
     matches = search_folders(folder, text)
+    match_count = 0
     for m in matches:
+        match_count += 1
         # print(m)
-        print('--------- MATCH -------------')
-        print('file: ' + m.file)
-        print('line: {}'.format(m.line))
-        print('match: ' + m.text.strip())
-        print()
+        # print('--------- MATCH -------------')
+        # print('file: ' + m.file)
+        # print('line: {}'.format(m.line))
+        # print('match: ' + m.text.strip())
+        # print()
+
+    print("Found {:,} matches.".format(match_count))
 
 
 def print_header():
@@ -50,23 +54,30 @@ def get_search_text_from_user():
 
 
 def search_folders(folder, text):
-    all_matches = []
+    # all_matches = []
     items = os.listdir(folder)
 
     for item in items:
         full_item = os.path.join(folder, item)
         if os.path.isdir(full_item):
-            matches = search_folders(full_item, text)
-            all_matches.extend(matches)
-        else:
-            matches = search_file(full_item, text)
-            all_matches.extend(matches)
+            # matches = search_folders(full_item, text)
+            # all_matches.extend(matches)
 
-    return all_matches
+            # for m in matches:
+            #     yield m
+            # yield from matches
+            yield from search_folders(full_item, text)
+        else:
+            yield from search_file(full_item, text)
+            # all_matches.extend(matches)
+            # for m in matches:
+            #     yield m
+
+    # return all_matches
 
 
 def search_file(filename, search_text):
-    matches = []
+    # matches = []
     with open(filename, 'r', encoding='utf-8') as fin:
 
         line_num = 0
@@ -74,9 +85,10 @@ def search_file(filename, search_text):
             line_num += 1
             if line.lower().find(search_text) >= 0:
                 m = SearchResult(line=line_num, file=filename, text=line)
-                matches.append(m)
+                # matches.append(m)
+                yield m
 
-        return matches
+        # return matches
 
 
 if __name__ == '__main__':

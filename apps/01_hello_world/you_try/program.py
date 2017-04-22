9223078e00ce797f254bd2790e4e2532064c4a1f
Luke Russell <lukerussell@hotmail.com.au>
2017-04-22 22:01:29 +1000

    App 1 finished

---
+++ apps/01_hello_world/you_try/program.py
@@ -0,0 +1,7 @@
+print('--------------------------------')
+print('          HELLO APP')
+print('--------------------------------')
+print()
+
+name = input('What is your name? ')
+print('Nice to meet you ' + name)
